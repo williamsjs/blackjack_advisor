40667253509c704aeb4001ddc60ceb071e671d7c
Scott Williams <williamsjs1224@gmail.com>
2015-05-06 15:51:27 -0400

    now I will try it with the correct file

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,12 +1,3 @@
-# soft_or_hard = { hard: {hit:  , stand:  , split:  , double_if_possible:  , double_if_possible_ows:  },
-#                  soft: { },
-#                  pair: { }
-#                }
-
-
-
-
-
 def valid_card?(card)
   if card.to_f == 0.0 || card.to_i <= 1
     false
@@ -17,27 +8,28 @@ end
 
 def get_user_cards
   user_cards = []
+  total = 0
   puts "Please enter your first card: "
   first_card = gets.chomp
-  puts "Please enter your second card: "
-  second_card = gets.chomp
-  total = 0
   if valid_card?(first_card) && first_card.upcase != 'A'
-    user_cards.push(first_card)
     total += first_card.to_i
+    user_cards.push(first_card)
   elsif first_card.upcase == 'A'
-    #function to add it to hash
+    #fucntion to add it to hash
   else
     puts "You must enter valid numbers or 'A'"
   end
+  puts "Please enter your second card: "
+  second_card = gets.chomp
   if valid_card?(second_card) && second_card.upcase != 'A'
-    user_cards.push(second_card)
     total += second_card.to_i
+    user_cards.push(second_card)
   elsif second_card.upcase == 'A'
     #function to add it to hash
   else
     puts "You must enter valid numbers or 'A'"
   end
+  return user_cards, total
 end
 
 def get_dealer_card
@@ -52,8 +44,9 @@ def get_dealer_card
   end
 end
 
+user_cards, total = get_user_cards
 dealer_card = get_dealer_card
-user_card_total = get_user_cards
 
-
-puts "The dealer's card is #{dealer_card} and your card total is #{user_card_total}"
+puts "total is #{total}"
+puts "your cards are #{user_cards}"
+puts "the dealer's card is #{dealer_card}"
