b73f3da6374b7ca8a190b202df5ebcdd54e35b66
Scott Williams <williamsjs1224@gmail.com>
2015-05-06 23:15:49 -0400

    added if statement controlling for ace and added :hard key

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -55,43 +55,214 @@ puts "your cards are #{user_cards}"
 puts "the dealer's card is #{dealer_card}"
 puts "All of the cards are #{all_cards}"
 
+#assumes that ace is not included in array of cards
+includes_ace = false
+
+#checks to see if ace is included and sets includes_ace to true
 if all_cards.include?('A')
-   hash_answer
+  includes_ace = true
 end
 
-
-def hit
-  puts "You should hit"
-end
-
-def stand
-  puts "You should stand"
-end
-
-def split
-  puts "You should split"
-end
-
-def double_otherwise_hit
-  puts "You should double, otherwise you should hit"
-end
-
-def double_otherwise_stand
-  puts "You should double, otherwise you should stand"
-end
-
-hash_answers = {hard: {5 => {2 => hit},
-                       3 => {5 => split},
-                       8 => {2 => stand},
-                       17 => {8 => double_otherwise_hit},
-                       10 => {9 => double_otherwise_stand}
+# sets up hash to include a hash
+hash_answers = {hard: {5 => {2 => "hit",
+                             3 => "hit",
+                             4 => "hit",
+                             5 => "hit",
+                             6 => "hit",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       6 => {2 => "hit",
+                             3 => "hit",
+                             4 => "hit",
+                             5 => "hit",
+                             6 => "hit",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       7 => {2 => "hit",
+                             3 => "hit",
+                             4 => "hit",
+                             5 => "hit",
+                             6 => "hit",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       8 => {2 => "hit",
+                             3 => "hit",
+                             4 => "hit",
+                             5 => "dh",
+                             6 => "dh",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       9 => {2 => "dh",
+                             3 => "dh",
+                             4 => "dh",
+                             5 => "dh",
+                             6 => "dh",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       10 => {2 => "dh",
+                             3 => "dh",
+                             4 => "dh",
+                             5 => "dh",
+                             6 => "dh",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       11 => {2 => "dh",
+                             3 => "dh",
+                             4 => "dh",
+                             5 => "dh",
+                             6 => "dh",
+                             7 => "dh",
+                             8 => "dh",
+                             9 => "dh",
+                             10 => "dh",
+                             11 => "dh"},
+                       12 => {2 => "hit",
+                             3 => "hit",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       13 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       14 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       15 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       16 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "hit",
+                             8 => "hit",
+                             9 => "hit",
+                             10 => "hit",
+                             11 => "hit"},
+                       17 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "stand",
+                             8 => "stand",
+                             9 => "stand",
+                             10 => "stand",
+                             11 => "stand"},
+                       18 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "stand",
+                             8 => "stand",
+                             9 => "stand",
+                             10 => "stand",
+                             11 => "stand"},
+                       19 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "stand",
+                             8 => "stand",
+                             9 => "stand",
+                             10 => "stand",
+                             11 => "stand"},
+                       20 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "stand",
+                             8 => "stand",
+                             9 => "stand",
+                             10 => "stand",
+                             11 => "stand"},
+                       21 => {2 => "stand",
+                             3 => "stand",
+                             4 => "stand",
+                             5 => "stand",
+                             6 => "stand",
+                             7 => "stand",
+                             8 => "stand",
+                             9 => "stand",
+                             10 => "stand",
+                             11 => "stand"}
                       },
-                soft: {}
+                soft: false
                 }
 
 
-hash_answers.each do |key, value|
-  if key == :hard
-    puts "Value is #{value}"
+if includes_ace == true
+  hash_answers[:hard].each do |key, value|
+
   end
 end
+
+if includes_ace == false
+  hash_answers[:soft].each do |key, value|
+
+  end
+end
+
+# hash_answers.each do |key, value|
+#     if key == :soft
+#       value.each do |key, value|
+#         if key == 10
+#           puts value
+#         end
+#       end
+#     elsif key == :hard
+#       value.each do |key, value|
+#         if key == 17
+#           puts value
+#         end
+#       end
+#     end
+# end
